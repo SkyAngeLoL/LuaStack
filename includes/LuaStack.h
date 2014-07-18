fabe1103b9782a87d44017c1b1ec265acbe6179d
Lena <SkyAngeLoL@yandex.ru>
2014-07-18 17:11:47 +0400

    Remove LuaStack.cpp

--- includes/LuaStack.h
+++ includes/LuaStack.h
@@ -17,39 +17,176 @@ extern "C" {
 class LuaStack {
 
 private:
+
 	lua_State *lua_state;
 
 public:
-	LuaStack();
-	~LuaStack();
+	
+	/*=============================
+		Constructor/Destructor
+	=============================*/
 
-	int RunFile(char* ScriptFileName);
+	LuaStack() {
+		lua_state = luaL_newstate();
 
-/* Def vars */
+		static const luaL_Reg lualibs[] = {
+				{"base", luaopen_base},
+				{"io", luaopen_io},
+				{NULL, NULL}
+		};
 
-	template<class type>
-	void AddVariable(type value, char* constantname);
+		for(const luaL_Reg *lib = lualibs; lib->func != NULL; lib++) {
+			luaL_requiref(lua_state, lib->name, lib->func, 1);
 
-/* Tables */
+			lua_settop(lua_state, 0);
+		}
 
-	void Table();
+	}
+	~LuaStack() {
+		lua_close(lua_state);
+	}
 
-	template<class type>
-	void AddVarInTable(type value, int index);
+	/*==================
+		Run lua file
+	==================*/
 
-	void RegTable(char* arrayname);
+	int LuaStack::RunFile(char* ScriptFileName) {
+		luaL_dofile(lua_state, ScriptFileName);
 
-/* Get arguments */
+		return lua_tointeger(lua_state, lua_gettop(lua_state));
+	}
 
-	int GetArgumentCount();
+	/*==============
+		Def vars
+	==============*/
+
+	void AddVariable(int value, char* constantname) {
+		lua_pushinteger(lua_state, value);
+
+		lua_setglobal(lua_state, constantname);
+	}
+
+	void AddVariable(double value, char* constantname) {
+		lua_pushnumber(lua_state, value);
+
+		lua_setglobal(lua_state, constantname);
+	}
+
+	void AddVariable(char* value, char* constantname) {
+		lua_pushstring(lua_state, value);
+
+		lua_setglobal(lua_state, constantname);
+	}
+
+	void AddVariable(bool value, char* constantname) {
+		lua_pushboolean(lua_state, value);
+
+		lua_setglobal(lua_state, constantname);
+	}
+
+	void AddVariable(lua_CFunction value, char* constantname) {
+		lua_pushcfunction(lua_state, value);
+
+		lua_setglobal(lua_state, constantname);
+	}
+
+	/*=============
+		Tables
+	=============*/
+
+	void Table() {
+		lua_createtable(lua_state, 2, 0);
+	}
+
+	void AddVarInTable(int value, int index) {
+		lua_pushnumber(lua_state, index);
+
+		lua_pushinteger(lua_state, value);
+
+		lua_settable(lua_state, -3);
+	}
+
+	void AddVarInTable(double value, int index) {
+		lua_pushnumber(lua_state, index);
+
+		lua_pushnumber(lua_state, value);
+
+		lua_settable(lua_state, -3);
+	}
+
+	void AddVarInTable(char* value, int index) {
+		lua_pushnumber(lua_state, index);
+
+		lua_pushstring(lua_state, value);
+
+		lua_settable(lua_state, -3);
+	}
+
+	void AddVarInTable(bool value, int index) {
+		lua_pushnumber(lua_state, index);
+
+		lua_pushboolean(lua_state, value);
+
+		lua_settable(lua_state, -3);
+	}
+
+	void AddVarInTable(lua_CFunction value, int index) {
+		lua_pushnumber(lua_state, index);
+
+		lua_pushcfunction(lua_state, value);
+
+		lua_settable(lua_state, -3);
+	}
+
+	/*====================
+		Get arguments
+	====================*/
+
+	int LuaStack::GetArgumentCount() {
+		return lua_gettop(lua_state);
+	}
 
 	template<class type>
 	type GetArgument(int index);
 
-/* Returns */
-
-	template<class type>
-	void Return(type value);
+	template<>
+	int GetArgument<int>(int index) {
+		return lua_tointeger(lua_state, index);
+	}
 	
+	template<>
+	double GetArgument<double>(int index) {
+		return lua_tonumber(lua_state, index);
+	}
+	
+	template<>
+	char* GetArgument<char*>(int index) {
+		return (char*)lua_tostring(lua_state, index);
+	}
+	
+	template<>
+	bool LuaStack::GetArgument<bool>(int index) {
+		return lua_toboolean(lua_state, index);
+	}
+
+	/*=============
+		Returns
+	=============*/
+
+	void Return(int value) {
+		lua_pushinteger(lua_state, value);
+	}
+
+	void Return(double value) {
+		lua_pushnumber(lua_state, value);
+	}
+
+	void Return(char* value) {
+		lua_pushstring(lua_state, value);
+	}
+
+	void Return(bool value) {
+		lua_pushboolean(lua_state, value);
+	}
 };
 
